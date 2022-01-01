7dc701dc6f024613b65178e4b7149442dabde47f
DucAnh21092000 <dinhanh21092000@gmail.com>
2022-01-01 22:16:51 +0700

    duc anh is me

--- js/animation.js
+++ js/animation.js
@@ -2,22 +2,42 @@ let initialValue = {
     year:"2022",
     content:"Happy New Year !",
     icon: " ❤❤❤❤",
-    mess:""
+    mess:"bạn Linh xinh gái"
 }
 
 const year = document.querySelector('.year')
 const content = document.querySelector('.content')
 const icon = document.querySelector('.icon')
+const mess = document.querySelector('.mess')
 
 let arrYear
 let arrContent=[]
 let arrIcon
+let arrMess
 arrYear= initialValue.year.split('')
 arrContent=(initialValue.content.split(""))
 arrIcon= initialValue.icon.split('')
+arrMess= initialValue.mess.split('')
+
+
 let arrYearResult=[]
 let arrContentResult=[]
 let arrIconResult=[]
+let arrMessResult=[]
+
+const displayMess = () => {
+    setTimeout( () =>{
+        const timeOut =setInterval( ()=> {
+            arrMessResult.push( arrMess.shift())           
+            mess.innerHTML = arrMessResult.join("")
+         
+            if(arrMess.length ==0){
+                clearInterval(timeOut)
+                displayIcon()
+            }
+        },1000)
+      },2000)
+}
 
 const displayContent = () => {
     setTimeout( () =>{
@@ -27,7 +47,7 @@ const displayContent = () => {
          
             if(arrContent.length ==0){
                 clearInterval(timeOut)
-                displayIcon()
+                displayMess()
             }
         },1000)
       },2000)
@@ -41,6 +61,7 @@ const displayIcon = () => {
          
             if(arrIcon.length ==0){
                 clearInterval(timeOut)
+                
             }
         },1000)
       },2000)
