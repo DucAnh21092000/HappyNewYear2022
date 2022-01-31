5abc7cb226abcce19db0398532ace13983ed3cc4
root <root@DESKTOP-28U2L2C.localdomain>
2022-01-31 14:49:47 +0700

    Đức Anh update chieu 29 tet

--- js/animation.js
+++ js/animation.js
@@ -1,24 +1,45 @@
+//          Tác giả : Đinh Đức Anh
+//          Mail    : anh.dd182330@sis.hust.edu.vn
+//                    dinhanh21092000@gmail.com
+
+// Object chứa giá trị cần hiển thị
 let initialValue = {
     year:"2022",
     content:"Happy New Year !",
     icon: " ❤❤❤❤",
-
+    mess:""
 }
 
 const year = document.querySelector('.year')
 const content = document.querySelector('.content')
 const icon = document.querySelector('.icon')
+const mess = document.querySelector('.mess')
 
-let arrYear
-let arrContent=[]
-let arrIcon
-arrYear= initialValue.year.split('')
-arrContent=(initialValue.content.split(""))
-arrIcon= initialValue.icon.split('')
+let arrYear= initialValue.year.split('')
+let arrContent=(initialValue.content.split(""))
+let arrIcon= initialValue.icon.split('')
+let arrMess= initialValue.mess.split('')
+
 let arrYearResult=[]
 let arrContentResult=[]
 let arrIconResult=[]
+let arrMessResult=[]
 
+// Hiển thị tin nhắn muốn hiển thị
+const displayMess = () => {
+    setTimeout( () =>{
+        const timeOut =setInterval( ()=> {
+            arrMessResult.push( arrMess.shift())           
+            mess.innerHTML = arrMessResult.join("")         
+            if(arrMess.length ==0){
+                clearInterval(timeOut)
+                displayIcon()
+            }
+        },1000) // thời gian lấy ra từng chữ 1 và hiển thị
+      },2000) // thời gian delay
+}
+
+// Hiển thị nội dung lời chúc
 const displayContent = () => {
     setTimeout( () =>{
         const timeOut =setInterval( ()=> {
@@ -27,12 +48,13 @@ const displayContent = () => {
          
             if(arrContent.length ==0){
                 clearInterval(timeOut)
-                displayIcon()
+                displayMess()
             }
         },1000)
       },2000)
 }
 
+// hiển thị icon
 const displayIcon = () => {
     setTimeout( () =>{
         const timeOut =setInterval( ()=> {
@@ -41,11 +63,13 @@ const displayIcon = () => {
          
             if(arrIcon.length ==0){
                 clearInterval(timeOut)
+                
             }
         },1000)
       },2000)
 }
 
+// function chạy đầu tiên
 function display() {
     setTimeout( () =>{
       const timeOut =setInterval( ()=> {
